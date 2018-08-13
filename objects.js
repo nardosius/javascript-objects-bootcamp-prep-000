a6242066355dc724413b2834c834413a67735c4f
nixonbii2@gmail.com <nixonbii2@gmail.com>
2018-08-13 23:58:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 var playlist = {MJ: "Beat It"};
 
-/*function updatePlaylist(obj, artist, title) {
+function updatePlaylist(obj, artist, title) {
   return Object.assign({}, obj, {[artist] : title});
-}*/
+}
 
 function updateObjectWithObject(targetObject, updatesObject) {
   return Object.assign({}, targetObject, updatesObject)
